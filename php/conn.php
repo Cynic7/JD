1bd36375e58230ec66cddf19d274649c455d5f76
qihuoyue <583516807@qq.com>
2018-10-06 20:33:26 +0800

    抓取了大量的数据，并渲染部分

--- php/conn.php
+++ php/conn.php
@@ -1,17 +1,14 @@
 <?php  
 	header('content-type:text/html;charset=utf-8');
-	//定义常量：值不能改变
 	define('HOSTNAME','localhost');
 	define('USERNAME','root');
-	define('PASSWORD','');
+	define('PASSWORD','leng1221');
 	$conn=@mysql_connect(HOSTNAME,USERNAME,PASSWORD);
 	if(!$conn){
 		die('数据库链接失败:'.mysql_error());
 	}
 
-	mysql_select_db('wrong');
+	mysql_select_db('jd_database');
 	mysql_query('SET NAMES UTF8');
-
-	
 
 ?>
--- php/del.php
+++
@@ -1,9 +0,0 @@
-<?php  
-	require "conn.php";
-
-	if(isset($_GET['id'])){
-		$sid=$_GET['id'];
-	}
-
-	mysql_query("delete from infomation where sid=$sid");
-?>
